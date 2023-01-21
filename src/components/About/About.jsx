bac63b777c33b5f0ca1444d6443d3fd12d4ac6e6
Moiquin5254G <112026738+Moiquin5254G@users.noreply.github.com>
2023-01-20 21:56:42 -0500

    Frist commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+export function About(){
+    return (
+        <>
+            <div>
+                <span>Texto de prueba</span>
+                <span>Texto de prueba x2</span>
+            </div>
+        </>
+    )
+}
